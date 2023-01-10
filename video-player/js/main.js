f9c8bee34011d2265427f1c3f70771c3174cda02
Amil Abdullazadeh <amil.abdullazadeh@vabiss.com>
2023-01-10 12:27:38 +0400

    video phase 2

--- video-player/js/main.js
+++ video-player/js/main.js
@@ -3,7 +3,7 @@ const toggleBtn = document.querySelector(".toggleButton");
 const progress = document.querySelector(".progress");
 const progressBar = document.querySelector(".progress__filled");
 const sliders = document.querySelectorAll(".controls__slider");
-// const skipBtns = document.querySelectorAll("[dta-skip]")
+const skipBtns = document.querySelectorAll("[data-skip]");
 
 function togglePlay() {
   if (video.paused || video.ended) {
@@ -18,9 +18,39 @@ function togglePlay() {
 toggleBtn.addEventListener("click", togglePlay);
 
 function handleProgress() {
-    const { currentTime, duration } = video
-    let progressPercantage = (currentTime / duration) * 100
-    progressBar.style.flexBasis = `${progressPercantage}%`
+  const { currentTime, duration } = video;
+  let progressPercantage = (currentTime / duration) * 100;
+  progressBar.style.flexBasis = `${progressPercantage}%`;
 }
- 
-video.addEventListener("timeupdate", handleProgress)
+
+video.addEventListener("timeupdate", handleProgress);
+
+//TODO:: keydown, keyup
+//TODO:: mousemove, mousedown, mouseup
+
+document.addEventListener("keydown", (event) => {
+  event.code === "Space" && togglePlay();
+});
+
+skipBtns.forEach((btn) => {
+  btn.addEventListener("click", handleSkip);
+});
+
+function handleSkip() {
+  video.currentTime += Number(this.dataset.skip);
+}
+
+let isMousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousedown", () => handleMouseEvent(true));
+progress.addEventListener("mousemove", (event) => isMousedown && scrub(event));
+progress.addEventListener("mouseup", () => handleMouseEvent(false));
+
+function handleMouseEvent(handleProps) {
+  isMousedown = handleProps;
+}
+
+function scrub(event) {
+  const scrubTime = (event.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
